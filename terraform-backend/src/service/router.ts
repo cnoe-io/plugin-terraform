8c55ba197d0402c7f0788844255c378b74361a4b
Nima Kaviani <nkaviani@amazon.com>
2023-08-15 21:56:05 -0700

    fix yarn tsc execution - remove unnecessary files - minor formatting - renaming cnoe -> cnoe-io for the front-end plugin scope - bump versions - renaming internal -> cnoe-io for the backend plugin scope - remove the config schema definition from the front-end module

--- terraform-backend/src/service/router.ts
+++ terraform-backend/src/service/router.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
-import { errorHandler } from '@backstage/backend-common';
+import {errorHandler} from '@backstage/backend-common';
 import express from 'express';
 import Router from 'express-promise-router';
-import { Logger } from 'winston';
-import { Config } from '@backstage/config';
-import { DefaultAwsCredentialsManager } from '@backstage/integration-aws-node';
-import { S3Client, ListObjectsV2Command, GetObjectCommand  } from "@aws-sdk/client-s3";
+import {Logger} from 'winston';
+import {Config} from '@backstage/config';
+import {DefaultAwsCredentialsManager} from '@backstage/integration-aws-node';
+import {S3Client, ListObjectsV2Command, GetObjectCommand} from "@aws-sdk/client-s3";
 
 type ListObjectsInput = {
   Bucket: string,
@@ -21,7 +21,7 @@ export interface RouterOptions {
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
-  const { logger, config } = options;
+  const {logger, config} = options;
   const awsCredentialsManager = DefaultAwsCredentialsManager.fromConfig(config);
   const credProvider = await awsCredentialsManager.getCredentialProvider({});
   const client = new S3Client({
@@ -33,18 +33,18 @@ export async function createRouter(
 
   router.get('/health', (_, response) => {
     logger.info('PONG!');
-    response.json({ status: 'ok' });
+    response.json({status: 'ok'});
   });
 
   router.post('/getFileList', async (req, res) => {
-    let responseObject:any = [];
-    let token:string|undefined = "1";
-    while(token) {
-      let input:ListObjectsInput = {
+    let responseObject: any = [];
+    let token: string | undefined = "1";
+    while (token) {
+      let input: ListObjectsInput = {
         Bucket: req.body.Bucket,
         Prefix: req.body.Key,
       }
-      if(token != "1" && token) {
+      if (token != "1" && token) {
         input.ContinuationToken = token;
       }
       const command = new ListObjectsV2Command(input);
@@ -57,23 +57,23 @@ export async function createRouter(
   });
 
   router.post('/getLocalFileList', async (req, res) => {
-    let responseObject:any[] = [];
+    let responseObject: any[] = [];
 
     try {
       const fsstat = fs.lstatSync(req.body.FileLocation);
-      if(fsstat.isDirectory()) {
+      if (fsstat.isDirectory()) {
         const filenames = fs.readdirSync(req.body.FileLocation);
-        for(let i in filenames) {
+        for (let i in filenames) {
           responseObject.push({
-            Key: req.body.FileLocation+"/"+filenames[i]
+            Key: req.body.FileLocation + "/" + filenames[i]
           });
         }
-      } else if(fsstat.isFile()) {
+      } else if (fsstat.isFile()) {
         responseObject.push({
           Key: req.body.FileLocation
         });
       }
-    } catch(e) {
+    } catch (e) {
       logger.error(e)
     }
 
@@ -81,20 +81,20 @@ export async function createRouter(
   });
 
   router.post('/getTFStateFile', async (req, res) => {
-    if(req.body.Bucket) {
+    if (req.body.Bucket) {
       const command = new GetObjectCommand({
         Bucket: req.body.Bucket,
         Key: req.body.Key,
       });
       const commandResponse = await client.send(command);
-      const str:any = await commandResponse.Body?.transformToString();
+      const str: any = await commandResponse.Body?.transformToString();
       res.json(JSON.parse(str));
     } else {
-      let jsonData:any = {};
+      let jsonData: any = {};
       try {
-        const data = fs.readFileSync(req.body.Key, { encoding: 'utf8', flag: 'r' });
+        const data = fs.readFileSync(req.body.Key, {encoding: 'utf8', flag: 'r'});
         jsonData = JSON.parse(data);
-      } catch(e) {
+      } catch (e) {
         logger.error(e);
       }
 
